5118b247c1acaa6d88ec941bc648c0e9eb54c15e
msanlub898 <msanlub898@g.educaand.es>
2023-10-06 20:43:35 +0200

    los 27

--- src/ejercicio27.py
+++ src/ejercicio27.py
@@ -1,18 +1,28 @@
-#Escribir un programa que pregunte el nombre el un producto, su precio y un número de unidades
-#y muestre por pantalla una cadena con el nombre del producto seguido de su precio unitario con
+#Escribir un programa que pregunte el nombre el un    nombre, su precio y un número de unidades
+#y muestre por pantalla una cadena con el nombre del    nombre seguido de su precio unitario con
 #6 dígitos enteros y 2 decimales, el número de unidades con tres dígitos y el coste total con 8 dígitos enteros y 2 decimales.
 
-def calculoNombre(nombre:str,unidad:int,precio:float,total:float) -> str:
-    '''función que da el nombre del producto''' 
-    return {nombre}: {unidad:2d} unidad x {precio:9.2f}€ = {total:11.2f}€.format(producto = producto, unidades = unidades, precio = precio, total = unidades * precio)
+def calculoTotal(nombre:str,precio:float,unidad:int) -> str:
+    '''función que define el total del ejercicio'''
+    total = float(precio*unidad)
+    return f'{nombre} -> {unidad:3d} unidades x{precio:9.2f}€ ={total:11.2f}€'
+        #unidad:3d -> soporta 3 digitos de unidades(ej: 300)
+        #precio:9.2f -> 9 caracteres(contando con los decimales y la coma) y el 2 el numero de decimales. La f es de float.
+        #total:11.2f -> 11 caracteres (contando con los decimales y la coma) y el 2 de numero de decimales. La f es de float.
 
+def mensajeSalida(totalProducto:str) -> str:
+    '''función que da el mensaje de salida'''
+    return "\t" + str(totalProducto)
 
 if __name__=="__main__":
     #entrada
-    nombre = str(input("Escribe el nombre del producto: "))
-    unidad = int(input("Escribe las unidades: "))
-    precio = float(input("Escribe el precio del producto: "))
-
+    nombre = str(input('Introduce el nombre del producto: '))
+    unidad = int(input('Introduce el número de unidades: '))
+    precio = float(input('Introduce el precio por unidad: '))
+  
     #procedimiento
+    totalProducto = calculoTotal(nombre,precio,unidad)
+    mensaje = mensajeSalida(totalProducto)
     
     #salida
+    print(mensaje)
